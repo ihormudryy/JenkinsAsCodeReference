4f8d54611eda9839c1e04ee0c0ef0f7904f2e24e
Ihor Mudryy <ihor.mudryy@here.com>
2018-04-19 13:28:25 +0200

    Fix creds issue

--- dockerizeit/master/groovy/credentials.groovy
+++ dockerizeit/master/groovy/credentials.groovy
@@ -10,6 +10,7 @@ import com.cloudbees.plugins.credentials.domains.*
 import com.cloudbees.plugins.credentials.impl.*
 import com.cloudbees.jenkins.plugins.sshcredentials.impl.*
 import hudson.plugins.sshslaves.*
+import org.jenkinsci.plugins.plaincredentials.*
 import org.jenkinsci.plugins.plaincredentials.impl.*
 
 // Read properties
