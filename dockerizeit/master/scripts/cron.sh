e51f3ec80c380d31e67b3edbb2a71a2565fe4d00
Ihor Mudryy <ihor.mudryy@gmail.com>
2018-09-06 15:11:26 +0000

    Chenged dockerfile to run Jenkins behind the reverse proxy

--- dockerizeit/master/scripts/cron.sh
+++ dockerizeit/master/scripts/cron.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -ex
 crontab <<EOF
-0 0 * * * "wget -O /usr/share/jenkins/jenkins.war http://updates.jenkins-ci.org/latest/jenkins.war && service jenkins start"
+0 0 * * * root "wget -O /usr/share/jenkins/jenkins.war http://updates.jenkins-ci.org/latest/jenkins.war && service jenkins restart"
 EOF
