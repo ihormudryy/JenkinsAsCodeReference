6273f08d172a71ef0cc5120ae849388d665c69c9
Ihor Mudryy <ihor.mudryy@here.com>
2018-09-28 16:59:39 +0200

    Fixed docker registry setup

--- dockerizeit/master/groovy/registry.groovy
+++ dockerizeit/master/groovy/registry.groovy
@@ -4,6 +4,8 @@ import org.jenkinsci.plugins.workflow.libs.*
 import org.jenkinsci.plugins.docker.commons.credentials.DockerRegistryEndpoint
 import org.jenkinsci.plugins.pipeline.modeldefinition.config.GlobalConfig
 
+def home_dir = System.getenv("JENKINS_HOME")
+def properties = new ConfigSlurper().parse(new File("$home_dir/jenkins.properties").toURI().toURL())
 GlobalConfig gc = GlobalConfig.get()
 docker_registry = new DockerRegistryEndpoint(properties.registry.registryURL, properties.registry.registryCredentials)
 gc.setRegistry(properties.registry.label)
